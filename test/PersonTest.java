500afc5d835de45821b3bfba51b0f08e709b709c
MsQQQQQ <ellenqiyalun@gmail.com>
2023-01-12 22:28:27 -0500

    resubmit Lab 0

--- test/PersonTest.java
+++ test/PersonTest.java
@@ -1,9 +1,8 @@
 
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
 
 
 /**
@@ -13,23 +12,35 @@ public class PersonTest {
 
   private Person john;
 
+  /**
+   * Setup all the variables before testing.
+   */
   @Before
   public void setUp() {
 
     john = new Person("John", "Doe", 1945);
   }
 
+  /**
+   * Test getFirstName() function.
+   */
   @Test
   public void testFirst() {
     assertEquals("John", john.getFirstName());
 
   }
 
+  /**
+   * Test getLastName() function.
+   */
   @Test
   public void testSecond() {
     assertEquals("Doe", john.getLastName());
   }
 
+  /**
+   * Test getYearOfBirth() function.
+   */
   @Test
   public void testYearOfBirth() {
     assertEquals(1945, john.getYearOfBirth());
